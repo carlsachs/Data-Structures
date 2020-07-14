e69d3f3789054fc037245b89fb60a24b77c682a8
carlsachs <sachscarl@gmail.com>
2020-07-14 11:08:21 -0400

    finished stacks and queues

--- stack/stack.py
+++ stack/stack.py
@@ -13,13 +13,14 @@ return elements in Last In First Out order.
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-
+        self.storage = []
     def __len__(self):
-        pass
-
+        return len(self.storage)
     def push(self, value):
-        pass
-
+        self.storage.append(value)
+        return self.storage
     def pop(self):
-        pass
+        if(self.storage == []):
+            return None
+        else:
+            return (self.storage.pop())
