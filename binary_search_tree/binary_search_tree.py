4fdf3ec97a3f11e36bde3ae7d14ea70fa2c295d8
carlsachs <sachscarl@gmail.com>
2020-07-16 10:49:41 -0400

    MVP day 2 homework

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -52,17 +52,26 @@ class BSTNode:
         else:
             if self.right is None:
                 return False
-            esle:
-            return self.right.contains(target)
+            else:
+                return self.right.contains(target)
 
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if self.right is None:
+            return self.value
+        else:
+            return self.right.get_max()
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        fn(self.value)
+
+        if self.left:
+            self.left.for_each(fn)
+
+        if self.right:
+            self.right.for_each(fn)
 
     # Part 2 -----------------------
 
