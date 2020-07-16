e35fa3fa2bb25939204b61071c1274f2e32a6501
carlsachs <sachscarl@gmail.com>
2020-07-16 13:04:58 -0400

    progress on BST day 2 MVP

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -58,27 +58,39 @@ class BSTNode:
 
     # Return the maximum value found in the tree
     def get_max(self):
-        if self.right is None:
-            return self.value
-        else:
-            return self.right.get_max()
+        max_value = self.value
+        current = self
+        while current:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.right
+        return max_value
 
     # Call the function `fn` on the value of each node
-    def for_each(self, fn):
-        fn(self.value)
+    def for_each(self, cb):
+        cb(self.value)
 
         if self.left:
-            self.left.for_each(fn)
+            self.left.for_each(cb)
 
         if self.right:
-            self.right.for_each(fn)
+            self.right.for_each(cb)
 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
-    def in_order_print(self, node):
-        pass
+    def in_order_print(self):
+        #if ther current node is None
+        # we know we've reached the end of a recursion (base case) and want to return
+        if self is None:
+            return
+        
+        # if check if we can move left
+        if self.left is not None:
+            self.left.in_order_print()
+
+        #
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
